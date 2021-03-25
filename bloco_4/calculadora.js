47f85c518cb2c3f2773183f1f6151f5840aca23a
Rafael Schmitz <schimitaum@gmail.com>
2021-03-25 18:25:40 -0300

    Exercicios

---
+++ bloco_4/calculadora.js
@@ -0,0 +1,28 @@
+let operacao = "%";
+let a = 15;
+let b = 5;
+
+switch(operacao){
+    case "+":
+    console.log(a+b);
+    break;
+
+    case "-":
+    console.log(a-b);
+    break;
+
+    case "*":
+    console.log(a*b);
+    break;
+
+    case "/":
+    console.log(a/b);
+    break;
+
+    case "%":
+    console.log(a%b);
+    break;
+
+    default:
+    console.log("Operação desconhecida")
+}
---
+++ bloco_4/triangulo.js
@@ -0,0 +1,14 @@
+let ang1 = 100;
+let ang2 = 50;
+let ang3 = 30;
+let triangulo;
+
+if (ang1<0 || ang2<0 || ang3<0){
+    console.log("um dos angulos é negativo");
+} else if (ang1 + ang2 + ang3 == 180){
+    triangulo = true;
+    console.log(triangulo);
+} else{
+    triangulo = false;
+    console.log(triangulo);
+}
